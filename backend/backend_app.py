39712063efc690ce08267b64a583c9ba709d79de
Omid444 <86495623+Omid444@users.noreply.github.com>
2025-06-16 15:08:14 +0200

    Post endpoit created

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,10 +10,39 @@ POSTS = [
 ]
 
 
+def validate_post_data(data):
+    if "title" not in data or "content" not in data:
+        return False
+    return True
+
+
 @app.route('/api/posts', methods=['GET'])
-def get_posts():
+def handle_get():
     return jsonify(POSTS)
 
+
+@app.route('/api/posts', methods=['POST'])
+def handle_posts():
+    """
+    Get POST requests and validate it,
+    if it is ok generate id and return json file,
+    otherwise return error.
+    log is created as well.
+    """
+    app.logger.info('GET request received for /api/books  POST log')  # Log a message
+    new_post = request.get_json()
+    if not validate_post_data(new_post):
+        return jsonify({"error": "Invalid post data"}), 400
+
+    new_id = max(book['id'] for book in POSTS) + 1
+    new_post['id'] = new_id
+    # Add the new post to our list
+    POSTS.append(new_post)
+    # Return the new post data to the client
+    return jsonify(new_post),201
+
+
+#@app.route('/api/posts/<id>', methods=['DELETE'])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
